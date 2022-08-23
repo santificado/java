4fbfe0992e1ddc8d7d777bffeb2ae7f7d7f44eb5
santificado <100155011+santificado@users.noreply.github.com>
2022-08-23 12:09:10 -0300

    Add files via upload

---
+++ app.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class app {
+    public static void main(String[] args) throws Exception{
+        Aluno aluno1 = new Aluno(2201, "Joao", 10);
+        Aluno aluno2 = new Aluno(2202, "Maria", 6);
+        Aluno aluno3 = new Aluno(2204, "Pedro", 5);
+
+        Array ListaAluno = new ArrayList();
+    }
+}
---
+++ colecoes.java
@@ -0,0 +1,24 @@
+public class colecoes {
+    private int rm;
+    private String nome;
+    private int nota;
+    public int getRm() {
+        return rm;
+    }
+    public void setRm(int rm) {
+        this.rm = rm;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public int getNota() {
+        return nota;
+    }
+    public void setNota(int nota) {
+        this.nota = nota;
+    }
+}
+   
